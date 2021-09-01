9454464a48513d66a6b47f42920d6d2645f2bcdd
Alex Tyler <alexander.liam.tyler@gmail.com>
2021-08-31 20:49:13 -0700

    W1D2 = evening Mac commit

--- fundamentals/oop/test-page.py
+++ fundamentals/oop/test-page.py
@@ -1,26 +1,13 @@
-def coins(money):
-    cash = money
-    quarters = 0
-    dimes = 0
-    nickels = 0
-    pennys = 0
-    print(cash)
-    for 
-        if cash - 25 >= 25:
-            cash = cash -25
-            quarters +=1
-        print(quarters)
-    """         elif cash - 10 >= 10:
-            cash = cash -10
-            dimes += 1
-        elif cash -5 >= 5:
-            cash = cash -5
-            nickels +=1
-        elif cash -1 >= 0:
-            cash = cash -1
-            pennys += 1
-        else:
-            print("")
-    list([quarters,dimes,nickels,pennys])"""
+# python code below!
+arra = [3,1,6,7,5]
 
-coins(87)
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        print(i)
+        if arr[i] > arr[i+1]:
+            arr[i],arr[i+1] = arr[i+1], arr[i]
+            return arr
+    print(arr)
+        
+
+bubble_sort(arra)
